cefcc5ad7bd0664bd6e751008015c06968510292
Thibault HENRY <thibhenry92@gmail.com>
2023-11-03 17:59:33 +0100

    Implement fetching user password from Secrets Manager

--- cmd/connect.go
+++ cmd/connect.go
@@ -17,7 +17,6 @@ var port string
 var region string
 var user string
 var localport string
-var iam bool
 
 // connectCmd represents the connect command
 var connectCmd = &cobra.Command{
@@ -29,7 +28,7 @@ Opens a connection with the given database @ the given port at localhost:5432
 	dbc connect
 
 Password Authentication:
-Use an existing database user password to authenticate against the dtaabase once the connection is open.
+Use an existing database user password to authenticate against the database once the connection is open.
 
 	dbc connect -h $host -u $user 
 
@@ -38,17 +37,7 @@ Then connect to the DB using the password for $user
 	psql -h localhost -p 5432 -U $user -d $database --password
 
 Then paste your password for $user.
-
-IAM Authentication: 
-Use IAM Authentication to authenticate as a User to the DB
-
-	dbc connect -h $host -u $user --iam
-
-This command will output a Token to use as a password when connecting to your database.	
-
-	psql -h localhost -p ${localport} -U ${user} -d ${database} --password
-
-Then paste the token`,
+`,
 
 	Run: func(cmd *cobra.Command, args []string) {
 		flag.Parse()
@@ -56,25 +45,22 @@ Then paste the token`,
 		// Assert AWS Creds
 		handler.AssertCredentials()
 
-		if host == "" {
-			host = handler.FuzzEndpoints(iam)
+		// if host == "" {
+		dbname, host := handler.FuzzEndpoints()
+		// }
+
+		if user == "" {
+			user, password := handler.FuzzUsers(dbname)
+			fmt.Println(user, password)
 		}
 
-		if iam {
-			fmt.Println("")
-			myFigure := figure.NewFigure("DBC Connect IAM", "", true)
-			myFigure.Print()
+		myFigure := figure.NewFigure("DBC Connect", "", true)
+		myFigure.Print()
 
-			fmt.Println("")
-			handler.GenerateToken(host, port, region, user)
-		} else {
-			myFigure := figure.NewFigure("DBC Connect", "", true)
-			myFigure.Print()
+		fmt.Println("")
 
-			fmt.Println("")
-		}
 		// Start Port-Forwarding Session
-		handler.Handler(region, host, port, localport)
+		handler.PortForward(region, host, port, localport)
 	},
 }
 
@@ -87,5 +73,4 @@ func init() {
 	connectCmd.Flags().StringVarP(&region, "region", "r", "eu-west-2", "Region of the Datbase to open a connection to (default eu-west-2)")
 	connectCmd.Flags().StringVarP(&user, "user", "u", "", "The DB User to open a connection with")
 	connectCmd.Flags().StringVarP(&localport, "localport", "l", "5432", "Local Port to forward database connection to (default 5432)")
-	connectCmd.Flags().BoolVarP(&iam, "iam", "I", false, "Bool: Use IAM Authentication for Database Connection - Generates a password token using IAM Authentication")
 }
